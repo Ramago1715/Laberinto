f7bdd59ff8420e59bb150f4716c844d6e5c20df6
Ramago1715 <74196893+Ramago1715@users.noreply.github.com>
2023-02-16 22:20:52 +0100

    Add files via upload

--- MainLaberinto.py
+++ MainLaberinto.py
@@ -1,11 +1,9 @@
 from MetodosLaberinto import *
 from colorama import Back,Fore
 
-
+muro = ["1,0","1,1","1,2","2,2","2,3","3,3","3,2","4,2"]
 
 tamaño = int(input("De cuanto quieres que sea el laberinto (n X n ) "))
-laberinto = generar_laberinto(tamaño)
-# printear_laberinto(laberinto)
-# print(Fore.RESET + Back.RESET)
-
-
+laberinto = generar_laberinto(tamaño,muro)
+printear_laberinto(laberinto)
+print(Fore.RESET + Back.RESET)
--- MetodosLaberinto.py
+++ MetodosLaberinto.py
@@ -4,13 +4,18 @@ from colorama import init,Back,Fore
 
 
 #Generar laberinto
-def generar_laberinto(tamaño):
+def generar_laberinto(tamaño,muros ):
     array = []
-    for x in tamaño:
-        print()
-        for y in tamañol: 
-            array.append([x,y])
-    print(array)  
+
+    for X in range(0,tamaño):
+        array.append([]) 
+        for Y in range(0,tamaño):
+            if (str(X) + "," +str(Y)) in muros:
+                
+                array[X].append("X")
+            else:
+                array[X].append(" ")
+    
     return array
   
 
@@ -26,13 +31,12 @@ def printear_laberinto(laberinto):
         print('\t')
         for celda in fila :
             if celda==" " :
-                print (Back.RED +Fore.BLACK + 'X ',end= " ")
+                print (Back.GREEN + Fore.BLACK+ '  '+Fore.RESET + Back.RESET,end= " ")
             elif celda == "X" :
-                print (Back.BLACK +Fore.BLACK + 'X ',end=" " )
+                print (Back.RED +Fore.BLACK + 'X '+ Fore.RESET + Back.RESET,end=" " )
             elif celda =="E":
-                print (Back.GREEN +Fore.GREEN + 'E ',end=" ")
+                print (Back.GREEN +Fore.GREEN + 'E '+ Fore.RESET + Back.RESET,end=" ")
             elif celda =="S":
-                print (Back.YELLOW +Fore.YELLOW + 'S ',end=" ")
+                print (Back.YELLOW +Fore.YELLOW + 'S '+ Fore.RESET + Back.RESET,end=" ")
             elif celda ==".":
-                print (Back.RED +Fore.BLACK + '. ',end=" ")
-            
+                print (Back.RED +Fore.BLACK + '. '+ Fore.RESET + Back.RESET,end=" ")

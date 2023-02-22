51d82dfd22adffb530a2c5627a9aef5f9140c5d4
Ramago1715 <74196893+Ramago1715@users.noreply.github.com>
2023-02-22 19:56:57 +0100

    Update MainLaberinto.py

--- MainLaberinto.py
+++ MainLaberinto.py
@@ -1,71 +1,18 @@
+#TODOS LOS IMPORTS
 from MetodosLaberinto import *
 from colorama import Back,Fore
 import time 
 
-muro = ["1,0","1,1","1,2","2,2","2,4","4,2"]
-entrada = ("0,0")
-camino = []
-salida = ("4,4")
-tamaño = int(input("De cuanto quieres que sea el laberinto (n X n ) "))
-X = 0
-Y = 0
-monigote = entrada
-# print(monigote)
-# Y =+ 1
-# monigote = str(X)+","+str(Y)
-# print(monigote)
+#DECLARACION DE VARIABLES
+camino: list[str] = []
+entrada: str = "0,0"
+monigote: str = entrada
+muro: list[str] = ["1,0","1,1","1,2","2,2","2,4","4,2"]
+salida:str = "4,4"
+salir:bool = False
+tamaño: int = int(input("De cuanto quieres que sea el laberinto (n X n ) "))
 
 
-salir = False
 laberinto,exterior = generar_laberinto(tamaño,muro,entrada,salida,camino)
 printear_laberinto(laberinto)
-print("\n")
-
-while salir == False:
-    atascado = True
-    if monigote == salida:
-      salir = True
-    #Ir a la derecha
-    elif str(X)+","+str(Y+1) not in muro and str(X)+","+str(Y+1) not in camino and str(X)+","+str(Y+1) not in exterior:
-       Y = Y+1
-       camino.append(str(X)+","+str(Y))
-       monigote = camino[-1]
-       laberinto,exterior = generar_laberinto(tamaño,muro,entrada,salida,camino)
-       printear_laberinto(laberinto)
-       time.sleep(1)
-       atascado = False
-       print("\n")
-    #Ir abajo
-    elif str(X+1)+","+str(Y) not in muro and str(X+1)+","+str(Y) not in camino and str(X+1)+","+str(Y) not in exterior:
-       X = X+1
-       camino.append(str(X)+","+str(Y))
-       monigote = camino[-1]
-       laberinto,exterior = generar_laberinto(tamaño,muro,entrada,salida,camino)
-       printear_laberinto(laberinto)
-       time.sleep(1)
-       atascado = False
-       print("\n")
-    #Ir a la Izquierda
-    elif str(X)+","+str(Y-1) not in muro and str(X)+","+str(Y-1) not in camino and str(X)+","+str(Y-1) not in exterior:
-       Y = Y-1
-       camino.append(str(X)+","+str(Y))
-       monigote = camino[-1]
-       laberinto,exterior = generar_laberinto(tamaño,muro,entrada,salida,camino)
-       printear_laberinto(laberinto)
-       time.sleep(1)
-       atascado = False
-       print("\n")
-    #IR arriba
-    elif str(X-1)+","+str(Y) not in muro and str(X-1)+","+str(Y) not in camino and str(X-1)+","+str(Y) not in exterior:
-       X = X-1
-       camino.append(str(X)+","+str(Y))
-       monigote = camino[-1]
-       laberinto,exterior = generar_laberinto(tamaño,muro,entrada,salida,camino)
-       printear_laberinto(laberinto)
-       time.sleep(1)
-       atascado = False
-       print("\n")
-    elif atascado == True:
-      muro.append(monigote)
-      monigote = entrada
-      camino = []
+movimiento_laberinto(salida,muro,tamaño,entrada,exterior,monigote,camino)

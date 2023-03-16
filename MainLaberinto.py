de3e1849baffc013a776d93951474364bc6842e3
Ramago1715 <74196893+Ramago1715@users.noreply.github.com>
2023-03-16 01:28:17 +0100

    Update MainLaberinto.py

--- MainLaberinto.py
+++ MainLaberinto.py
@@ -4,15 +4,16 @@ from colorama import Back,Fore
 import time 
 
 #DECLARACION DE VARIABLES
+movimiento = []
 camino: list[str] = []
 entrada: str = "0,0"
 monigote: str = entrada
-muro: list[str] = ["1,0","1,1","1,2","2,2","2,4","4,2"]
-salida:str = "4,4"
+muro: list[str] = ["1,1","0,1","2,2","3,2","1,3","4,4"]
+salida:str = "0,3"
 salir:bool = False
 tamaño: int = int(input("De cuanto quieres que sea el laberinto (n X n ) "))
 
 
 laberinto,exterior = generar_laberinto(tamaño,muro,entrada,salida,camino)
 printear_laberinto(laberinto)
-movimiento_laberinto(salida,muro,tamaño,entrada,exterior,monigote,camino)
+movimiento_laberinto(salida,muro,tamaño,entrada,exterior,monigote,camino,movimiento)
